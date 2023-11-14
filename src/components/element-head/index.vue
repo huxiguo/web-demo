5c7da216bd2667f380c471be70c639cd86c69ae8
huxiguo <hu@seekhoo.cn>
2023-11-14 21:00:49 +0800

    import antdv

---
+++ src/components/element-head/index.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts"></script>
+<template>
+  <div>qweqweqeqw</div>
+</template>
---
+++ src/views/element-head-demo/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+<template>
+  <a-layout>
+    <a-layout-header class="header"></a-layout-header>
+    <a-layout-content class="content">Content</a-layout-content>
+    <a-layout-footer class="footer">Footer</a-layout-footer>
+  </a-layout>
+</template>
+
+<style scoped>
+.header {
+  height: 40px;
+  width: 100%;
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+</style>
